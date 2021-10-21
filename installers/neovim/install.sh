21eb84644345957f9ede2695788f547f8463a9fb
DenRen <denrenruslan@gmail.com>
2021-10-22 00:49:13 +0300

    Fixed micro buf with -p flag in neovim

--- installers/neovim/install.sh
+++ installers/neovim/install.sh
@@ -4,5 +4,5 @@ sudo apt-get install -y neovim
 curl 	-fLo ~/.local/share/nvim/site/autoload/plug.vim \
 	--create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-mkdir ~/.config/nvim
+mkdir -p ~/.config/nvim
 ln -s ~/.vimrc ~/.config/nvim/init.vim
