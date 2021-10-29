9e4c33a76e0c86752146b6ab03914c76fac83248
DenRen <denrenruslan@gmail.com>
2021-10-29 23:07:26 +0300

    Пофиксил баг в установщике плагинов для vim

--- installers/vimrc/install.sh
+++ installers/vimrc/install.sh
@@ -1,15 +1,30 @@
-#/bin/bash
+#/bin/bash -x
 
 CURRENT_DIR="$(dirname $0)"
 
-if [ -e "~/.vimrc" ]; then
-	echo "Save old .vimrc in current dir: ${CURRENT_DIR}"
-	cp ~/.vimrc "${CURRENT_DIR}"/.vimrc_save
-fi
+save_old () {
+    local path="$1"
+    local name="$2"    
+
+    local fullPath="${path}/${name}"
+
+    if [ -e ${fullPath} ] || [ -L ${fullPath} ]; then
+	    echo "Save old ${fullPath} in current dir: ${CURRENT_DIR}"
+	    mv ${fullPath} ${CURRENT_DIR}/${name}_save
+    fi
+}
+
+
+save_old ~ .vimrc
+save_old ~ .gvimrc
 
 curl -fLo ~/.vim/autoload/plug.vim \
      --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-cp "${CURRENT_DIR}"/.vimrc ~/.vimrc
+# Install all necessaries programms
+sudo apt install -y ripgrep fzf
 
-echo -e "\nEnter in nvim :PlugInstall"
+cp "${CURRENT_DIR}"/.vimrc ~/.vimrc
+ln -s ~/.vimrc ~/.gvimrc
+
+echo "\nEnter in all vims (vim, nvim, gvim) :PlugInstall"
